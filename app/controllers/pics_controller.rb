3ed6db4e285389462cf8f50f2da0d1ea91439b97
Patrick Bentley <pbentley07@gmail.com>
2017-08-28 10:20:19 -0400

    Fixed Form Submit Issue

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -40,7 +40,7 @@ class PicsController < ApplicationController
   private
 
   def pic_params
-    params.require(:pic).permit(:title,:description)
+    params.require(:pic).permit(:title,:description, :user_id)
   end
 
   def find_pic
