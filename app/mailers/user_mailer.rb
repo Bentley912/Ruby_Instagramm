e970cf2f98b1c83998d8f83bc63160cf80d1b179
Patrick Bentley <pbentley07@gmail.com>
2018-01-17 22:08:30 -0500

    Added Mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,7 +4,7 @@ class UserMailer < ApplicationMailer
   def welcome_email(user)
     @user = user
     @url  = 'http://example.com/login'
-    mail(to: @user.email, subject: 'Welcome to My Awesome Site')
+    mail(to: @user.email, subject: 'Welcome to Ruby Instagramm')
   end
-  
+
 end
